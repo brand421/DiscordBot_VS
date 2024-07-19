87a0d75113312174b322d60b022d19088f6e4193
brand421 <r3v9191@gmail.com>
2024-07-18 21:50:49 -0400

    Add project files.

---
+++ DiscordBot/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Commands;
+
+
+namespace DiscordBot
+{
+    internal class Commands
+    {
+        
+        CommandsExtension commandsExtension = client
+
+        [Command("score")]
+
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        public class Client
+        {
+            //DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault("MTI2MDI0OTgwMDM0ODAwODQ1OA.Gl_tlt.-y50U7e_9PPDv0v1232EQ0OxTNkkSogCngmo_I", DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents | DiscordIntents.GuildVoiceStates);
+            //DiscordClient client = builder.Build()
+        }
+        static async Task Main(string[] args)
+        {
+            DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault("MTI2MDI0OTgwMDM0ODAwODQ1OA.Gl_tlt.-y50U7e_9PPDv0v1232EQ0OxTNkkSogCngmo_I", DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents | DiscordIntents.GuildVoiceStates);
+            DiscordClient client = builder.Build();
+
+            client.UseVoiceNext();
+
+            DiscordChannel channel;
+            VoiceNextConnection connection = await channel.ConnectAsync();
+
+            builder.ConfigureEventHandlers
+            (
+                b => b.HandleMessageCreated(async (s, e) =>
+                {
+                    if (e.Message.Content.ToLower().StartsWith("dude"))
+                    {
+                        await e.Message.RespondAsync("score!");
+                    }
+                })
+            );
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
