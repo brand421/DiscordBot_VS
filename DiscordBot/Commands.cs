ea044046d17f96bf459767f23a020c62082ba381
brand421 <r3v9191@gmail.com>
2024-07-20 16:49:32 -0400

    created initial framework for command error handling

--- DiscordBot/Commands.cs
+++ DiscordBot/Commands.cs
@@ -4,16 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus.Commands;
+using DSharpPlus;
 
 
 namespace DiscordBot
 {
-    internal class Commands
+    internal class BandCommand
     {
-        
-        CommandsExtension commandsExtension = client
 
-        [Command("score")]
-
+        [Command("band")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("Y’know, like, this band sucks. But it’s like, they suck like, in new ways, y’know.Like, they suck in ways we haven’t, like, seen stuff suck before");
     }
+
+    internal class LincolnCommand
+    {
+        // NEED TO ADD ABILITY TO ADD NAME TO COMMAND
+        [Command("lincoln")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("When I almost scored with four chicks seven years ago, this one chick’s father brought forth upon me a prostitute. Huh-huh. Because some dudes just weren’t created equal. Huh-huh");
+    }
+
+    internal class WorkCommand
+    {
+        // NEED TO ADD ABILITY TO ADD NAME TO COMMAND
+        [Command("work")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("Work sucks, let's go break something");
+    }
+
+    internal class BallsCommand
+    {
+        // NEED TO ADD ABILITY TO ADD NAME TO COMMAND
+        [Command("balls")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("Beavis, your balls are filthy. Take them to the ball washer immediately");
+    }
+
+    internal class InsideOutCommand
+    {
+        // NEED TO ADD ABILITY TO ADD NAME TO COMMAND
+        [Command("kickass")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("Shut up Beavis before I kick your ass so hard it’ll turn inside-out and come out your mouth.");
+    }
+    internal class RocksCommand
+    {
+        // NEED TO ADD ABILITY TO ADD NAME TO COMMAND
+        [Command("rock")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("I've got a rock formation in my pants");
+    }
+
+    internal class listCommand
+    {
+        [Command("list")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("My list includes any girl with at least one boob");
+    }
+
+    internal class helloCommand
+    {
+        [Command("hello")]
+        public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync("Hey losers. huh huh");
+    }
+
 }
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -3,6 +3,14 @@ using System.Net.NetworkInformation;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.VoiceNext;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.TextCommands;
+using DSharpPlus.Commands.Processors.TextCommands.Parsing;
+using DSharpPlus.AsyncEvents;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Diagnostics.Tracing;
+
 
 namespace DiscordBot
 {
@@ -10,20 +18,22 @@ namespace DiscordBot
     {
         static async Task Main(string[] args)
         {
-            // DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(
-            //     "MTI2MDI0OTgwMDM0ODAwODQ1OA.Gl_tlt.-y50U7e_9PPDv0v1232EQ0OxTNkkSogCngmo_I",
-            //     DiscordIntents.AllUnprivileged
-            //         | DiscordIntents.MessageContents
-            //         | DiscordIntents.GuildVoiceStates
-            // );
-            // DiscordClient client = builder.Build();
-
-            Client.client client = new Client();
+             DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(
+                 "MTI2MDI0OTgwMDM0ODAwODQ1OA.Gl_tlt.-y50U7e_9PPDv0v1232EQ0OxTNkkSogCngmo_I",
+                    DiscordIntents.AllUnprivileged
+                    | DiscordIntents.MessageContents
+                    | DiscordIntents.GuildVoiceStates
+             );
+             DiscordClient client = builder.Build();
 
             client.UseVoiceNext();
 
-            DiscordChannel channel;
-            VoiceNextConnection connection = await channel.ConnectAsync();
+            //DiscordChannel channel;
+            //if (channel != null)
+            //{
+            //    VoiceNextConnection connection = await channel.ConnectAsync();
+            //}
+            
 
             builder.ConfigureEventHandlers(b =>
                 b.HandleMessageCreated(
@@ -35,15 +45,37 @@ namespace DiscordBot
                             }
                         }
                     )
-                    .UserJoined(
-                        async (s, e) =>
-                        {
-                            if (e.User.Username == "r3vo1ution") { }
-                        }
-                    )
             );
 
-            await client.ConnectAsync();
+            CommandsExtension commandsExtension = client.UseCommands(new CommandsConfiguration()
+            {
+                RegisterDefaultCommandProcessors = true,
+                UseDefaultCommandErrorHandler = false
+            });
+
+            commandsExtension.CommandErrored += async (s, e) =>
+            {
+                StringBuilder stringBuilder = new();
+                stringBuilder.Append("You buttmunch, ");
+                stringBuilder.Append(e.Exception.GetType().Name);
+                stringBuilder.Append(" doesn't exist");
+                stringBuilder.Append(DSharpPlus.Formatter.InlineCode(DSharpPlus.Formatter.Sanitize(e.Exception.Message)));
+
+                await eventArgs.Context.RespondAsync(stringBuilder);
+            };
+
+            commandsExtension.AddCommands(typeof(Program).Assembly);
+            TextCommandProcessor textCommandProcessor = new(new()
+            {
+                PrefixResolver = new DefaultPrefixResolver(true, "!", "&").ResolvePrefixAsync
+            });
+
+            await commandsExtension.AddProcessorsAsync(textCommandProcessor);
+
+
+            DiscordActivity status = new("trying to score", DiscordActivityType.Custom);
+
+            await client.ConnectAsync(status, DiscordUserStatus.Online);
             await Task.Delay(-1);
         }
     }
